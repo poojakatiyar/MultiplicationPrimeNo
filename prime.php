a5cfe316826579ba77caffc8ae63f2942958077d
poojakatiyar <pooja.94katiyar@gmail.com>
2019-05-09 15:33:35 +0530

    my first commit

---
+++ prime.php
@@ -0,0 +1,55 @@
+
+
+    <?php
+
+    /*
+    * To change this template, choose Tools | Templates
+    * and open the template in the editor.
+    */
+
+    $n= 10; 
+   // $matrix=  array();
+    for($i=1;$i<=$n;$i++){  //numbers to be checked as prime
+
+    $counter = 0; 
+    for($j=1;$j<=$i;$j++){ //all divisible factors
+
+
+    if($i % $j==0){ 
+
+    $counter++;
+    }
+    }
+
+    //prime requires 2 rules ( divisible by 1 and divisible by itself)
+    if($counter==2){
+   $primeno[] =   $i;
+
+    
+
+
+
+    }
+    }
+    ?>
+
+<?php 
+    foreach ($primeno as $key => $value) {
+ echo $value;   
+}
+
+//for ($r = 1; $r <= $i; $r++)
+//
+//    {
+//
+//    for ($c = 1; $c <= $i;$c++)
+//
+//    {
+//
+//    echo $arr[$r][$c]=$r*$c;
+//    echo " "; 
+//    }
+//    echo "</br>";
+//   
+//    }
+?>
